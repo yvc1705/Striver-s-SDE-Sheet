6fce370badc8d3cfbcc8f107724f50011f059032
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2023-12-22 22:40:24 +0530

    Add files via upload

---
+++ kadanes_subarray_max_sum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        System.out.println("The maximum subarray sum is: " + maxSubArray(nums));
+    }
+}
