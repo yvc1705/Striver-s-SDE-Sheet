9fa4c30214d3f11fa4d2735201371c71c199edae
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2023-12-24 19:07:52 +0530

    Add files via upload

---
+++ Sort_colors.java
@@ -0,0 +1,24 @@
+public class /*Solution*/ Sort_colors {
+    public void sortColors(int[] nums) {
+        int red = 0, white = 0, blue = nums.length - 1;
+        
+        while (white <= blue) {
+            if (nums[white] == 0) {
+                swap(nums, red, white);
+                red++;
+                white++;
+            } else if (nums[white] == 1) {
+                white++;
+            } else {
+                swap(nums, white, blue);
+                blue--;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
