a31f7862cd7137ab4ebeec13cc9ed8440c9b54ed
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2023-12-27 21:38:34 +0530

    Add files via upload

---
+++ pow_X_N.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double result = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+        return result;
+    }
+}
