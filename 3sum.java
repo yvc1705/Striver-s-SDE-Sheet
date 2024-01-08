910a08cf6c1c02fbbecbb7939ffedad49de5fec0
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-08 22:41:10 +0530

    Add files via upload

---
+++ 3sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1, right = nums.length - 1, sum = 0 - nums[i];
+                
+                while (left < right) {
+                    if (nums[left] + nums[right] == sum) {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < sum) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
