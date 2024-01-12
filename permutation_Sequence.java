d03e7d3cabd10b7579df3530289b8f5344335a10
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-12 23:58:07 +0530

    Add files via upload

---
+++ permutation_Sequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n + 1];
+        StringBuilder sb = new StringBuilder();
+
+        int sum = 1;
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+            factorial[i] = sum;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+
+        k--;
+
+        for (int i = 1; i <= n; i++) {
+            int index = k / factorial[n - i];
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            k -= index * factorial[n - i];
+        }
+
+        return sb.toString();
+    }
+}
