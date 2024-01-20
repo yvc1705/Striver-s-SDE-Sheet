f39b164769d69944ca64b4366585fe5b87e985df
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-20 23:43:46 +0530

    Add files via upload

---
+++ Lur_cache.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class LRUCache {
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+    }
+
+    private Map<Integer, Node> map;
+    private Node head, tail;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            add(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            add(node);
+        } else {
+            if (map.size() == capacity) {
+                map.remove(tail.prev.key);
+                remove(tail.prev);
+            }
+            Node newNode = new Node();
+            newNode.key = key;
+            newNode.value = value;
+            map.put(key, newNode);
+            add(newNode);
+        }
+    }
+
+    private void add(Node node) {
+        Node headNext = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = headNext;
+        headNext.prev = node;
+    }
+
+    private void remove(Node node) {
+        Node nextNode = node.next;
+        Node prevNode = node.prev;
+        nextNode.prev = prevNode;
+        prevNode.next = nextNode;
+    }
+}
