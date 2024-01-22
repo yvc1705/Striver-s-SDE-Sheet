e2bc87b30d3b3fa3d4275a6f537b29fef9d57c49
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-22 23:20:38 +0530

    Add files via upload

---
+++ rotting_oranges.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int freshOranges = 0;
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    freshOranges++;
+                } else if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int minutes = 0;
+
+        while (!queue.isEmpty() && freshOranges > 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                for (int[] dir : directions) {
+                    int x = current[0] + dir[0];
+                    int y = current[1] + dir[1];
+                    if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2;
+                        freshOranges--;
+                        queue.offer(new int[]{x, y});
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return freshOranges == 0 ? minutes : -1;
+    }
+}
