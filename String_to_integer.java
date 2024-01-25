f7a8018117935d952f4b6c4e99bc4930d96730e0
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-25 23:40:36 +0530

    Add files via upload

---
+++ String_to_integer.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int myAtoi(String s) {
+        int index = 0;
+        int sign = 1;
+        int result = 0;
+        
+        // Remove leading whitespaces
+        while (index < s.length() && s.charAt(index) == ' ') {
+            index++;
+        }
+        
+        // Check for sign
+        if (index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
+            sign = (s.charAt(index++) == '-') ? -1 : 1;
+        }
+        
+        // Convert digits to integer
+        while (index < s.length() && Character.isDigit(s.charAt(index))) {
+            int digit = s.charAt(index) - '0';
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + digit;
+            index++;
+        }
+        
+        return sign * result;
+    }
+}
