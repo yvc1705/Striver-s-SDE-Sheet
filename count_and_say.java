72721e6c66cf9cca10867b3a1e113a6c447061b2
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-27 23:39:50 +0530

    Add files via upload

---
+++ count_and_say.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String prev = countAndSay(n - 1);
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < prev.length(); i++) {
+            if (i + 1 < prev.length() && prev.charAt(i) == prev.charAt(i + 1)) {
+                count++;
+            } else {
+                result.append(count).append(prev.charAt(i));
+                count = 1;
+            }
+        }
+        return result.toString();
+    }
+}
