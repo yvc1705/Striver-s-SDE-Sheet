4ee00f0947669249a5692dacf939aeb7b36499a1
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2024-01-30 21:12:07 +0530

    Add files via upload

---
+++ BInary_tree_preorder_transversal_2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorder(root, result);
+        return result;
+    }
+
+    private void postorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        postorder(node.left, result);
+        postorder(node.right, result);
+        result.add(node.val);
+    }
+}
